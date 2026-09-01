2724415cf990ed6da0ade16fbfe7598333a25e00
Andreas Ziegler <062.127@gmail.com>
2026-08-31 22:19:08 -0400

    Updated neovim config.

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -7,6 +7,19 @@ return {
       },
       ccls = {
         mason = vim.fn.executable("clangd") == 0,
+        init_options = {
+          cache = {
+            directory = '/tmp/ccls-cache',
+          },
+        },
+      },
+      ruff = {
+        cmd_env = { RUFF_TRACE = "messages" },
+        init_options = {
+          settings = {
+            logLevel = "error",
+          },
+        },
       },
     },
   },
